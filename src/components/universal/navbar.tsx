d319b273a6398b6f8d978e21c6229c4091e3e144
Belsetbut <146377681+Belsetbut@users.noreply.github.com>
2024-10-22 14:08:21 +0000

    muss jetzt heim gl

--- src/components/universal/navbar.tsx
+++ src/components/universal/navbar.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import Link from "next/link";
 import Image from "next/image";
 import {
@@ -9,14 +11,34 @@ import {
   NavigationMenuTrigger,
   navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu";
-import { SproutIcon } from "lucide-react";
+import { SproutIcon, Menu } from "lucide-react";
 import { cn } from "@/lib/utils";
-
 import * as React from "react";
+import { motion } from "framer-motion"; // Framer Motion import
 
 export function Navbar() {
+  const [isMenuOpen, setMenuOpen] = React.useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(prev => !prev);
+  };
+
+  // Schließen des Menüs, wenn außerhalb geklickt wird
+  React.useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (isMenuOpen && !event.target.closest('.dropdown-menu')) {
+        setMenuOpen(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [isMenuOpen]);
+
   return (
-    <header className="bg-[#1a5f1a] text-white shadow-md">
+    <header className="bg-[#1a5f1a] text-white shadow-md relative">
       <nav className="container mx-auto px-4 py-4 flex items-center">
         <Link href="/" className="text-2xl font-bold flex items-center mr-auto">
           <Image
@@ -29,107 +51,103 @@ export function Navbar() {
           SV Schloßberg
         </Link>
 
-        <NavigationMenu>
-          <NavigationMenuList>
-            <NavigationMenuItem>
-              <NavigationMenuTrigger>Sportarten</NavigationMenuTrigger>
-              <NavigationMenuContent>
-                <ul className="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
-                  <li className="row-span-3">
-                    <NavigationMenuLink asChild>
-                      <Link
-                        className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
-                        href="/"
-                      >
-                        <SproutIcon className="h-6 w-6" />
-                        <div className="mb-2 mt-4 text-lg font-medium">
-                          Finde deine Sportart
-                        </div>
-                        <p className="text-sm leading-tight text-muted-foreground">
-                          Finde mit unsererm Interactiven Sportsfinder deine
-                          Sportart. Beantworte ein par fragen un erhalte in
-                          wenigen Minuten den perfekten Sport
-                        </p>
-                      </Link>
-                    </NavigationMenuLink>
-                  </li>
-                  <ListItem href="/docs" title="Introduction">
-                    Re-usable components built using Radix UI and Tailwind CSS.
-                  </ListItem>
-                  <ListItem href="/docs/installation" title="Installation">
-                    How to install dependencies and structure your app.
-                  </ListItem>
-                  <ListItem href="/docs/installation" title="Installation">
-                    How to install dependencies and structure your app.
-                  </ListItem>
-                  <ListItem href="/docs/installation" title="Installation">
-                    How to install dependencies and structure your app.
-                  </ListItem>
-                  <ListItem href="/docs/installation" title="Installation">
-                    How to install dependencies and structure your app.
-                  </ListItem>
-                  <ListItem
-                    href="/docs/primitives/typography"
-                    title="Typography"
-                  >
-                    Styles for headings, paragraphs, lists...etc
-                  </ListItem>
-                </ul>
-              </NavigationMenuContent>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-              <Link href="/verein" className={navigationMenuTriggerStyle()}>
-                Verein
-              </Link>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-              <Link href="/kontakt" className={navigationMenuTriggerStyle()}>
-                Kontakt
-              </Link>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-              <Link href="/docs" legacyBehavior passHref>
-                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                  Sponsoren
-                </NavigationMenuLink>
-              </Link>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-              <Link href="/docs" legacyBehavior passHref>
-                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                  Sponsoren
-                </NavigationMenuLink>
-              </Link>
-            </NavigationMenuItem>
-          </NavigationMenuList>
-        </NavigationMenu>
+        <div className="hidden lg:block">
+          <NavigationMenu>
+            <NavigationMenuList>
+              <NavigationMenuItem>
+                <NavigationMenuTrigger>Sportarten</NavigationMenuTrigger>
+                <NavigationMenuContent>
+                  <ul className="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
+                    <li className="row-span-3">
+                      <NavigationMenuLink asChild>
+                        <Link
+                          className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
+                          href="/"
+                        >
+                          <SproutIcon className="h-6 w-6" />
+                          <div className="mb-2 mt-4 text-lg font-medium">
+                            Finde deine Sportart
+                          </div>
+                          <p className="text-sm leading-tight text-muted-foreground">
+                            Finde mit unsererm Interactiven Sportsfinder deine
+                            Sportart. Beantworte ein paar Fragen und erhalte in
+                            wenigen Minuten den perfekten Sport.
+                          </p>
+                        </Link>
+                      </NavigationMenuLink>
+                    </li>
+                    {/* Weitere ListItems hier... */}
+                  </ul>
+                </NavigationMenuContent>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <Link href="/verein" className={navigationMenuTriggerStyle()}>
+                  Verein
+                </Link>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <Link href="/kontakt" className={navigationMenuTriggerStyle()}>
+                  Kontakt
+                </Link>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <Link href="/docs" legacyBehavior passHref>
+                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                    Sponsoren
+                  </NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+            </NavigationMenuList>
+          </NavigationMenu>
+        </div>
+
+        <button className="lg:hidden ml-4" onClick={toggleMenu}>
+          <motion.div
+            animate={{ rotate: isMenuOpen ? 90 : 0 }}
+            transition={{ duration: 0.3 }}
+          >
+            <Menu />
+          </motion.div>
+        </button>
+
+        {isMenuOpen && (
+          <div className="absolute top-16 right-0 bg-[#1a5f1a] w-full text-left shadow-lg z-50 dropdown-menu flex">
+            <NavigationMenu>
+              <ul className="flex flex-col w-full">
+                <li>
+                  <NavigationMenuLink asChild>
+                    <Link href="/" className={`${navigationMenuTriggerStyle()} text-center w-full`}>
+                      Sportarten
+                    </Link>
+                  </NavigationMenuLink>
+                </li>
+                <li>
+                  <NavigationMenuLink asChild>
+                    <Link href="/verein" className={`${navigationMenuTriggerStyle()} text-center w-full`}>
+                      Verein
+                    </Link>
+                  </NavigationMenuLink>
+                </li>
+                <li>
+                  <NavigationMenuLink asChild>
+                    <Link href="/kontakt" className={`${navigationMenuTriggerStyle()} text-center w-full`}>
+                      Kontakt
+                    </Link>
+                  </NavigationMenuLink>
+                </li>
+                <li>
+                  <NavigationMenuLink asChild>
+                    <Link href="/docs" className={`${navigationMenuTriggerStyle()} text-center w-full`}>
+                      Sponsoren
+                    </Link>
+                  </NavigationMenuLink>
+                </li>
+                {/* Weitere Links hier... */}
+              </ul>
+            </NavigationMenu>
+          </div>
+        )}
       </nav>
     </header>
   );
 }
-
-const ListItem = React.forwardRef<
-  React.ElementRef<"a">,
-  React.ComponentPropsWithoutRef<"a">
->(({ className, title, children, ...props }, ref) => {
-  return (
-    <li>
-      <NavigationMenuLink asChild>
-        <a
-          ref={ref}
-          className={cn(
-            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
-            className
-          )}
-          {...props}
-        >
-          <div className="text-sm font-medium leading-none">{title}</div>
-          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
-            {children}
-          </p>
-        </a>
-      </NavigationMenuLink>
-    </li>
-  );
-});
-ListItem.displayName = "ListItem";

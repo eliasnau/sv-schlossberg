a1fbcc9f619b74a3951ba739b7d73c537e37fc22
Elias Nau <elias.nau@outlook.de>
2024-10-25 21:59:32 +0200

    feat: add 404 page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,10 @@
+export default function NotFound() {
+  return (
+    <main className="flex items-center justify-center">
+      <div className="container mx-auto p-4">
+        <h1 className="text-2xl font-bold mb-4">404 - Seite nicht gefunden</h1>
+        <p>Die angeforderte Seite existiert nicht.</p>
+      </div>
+    </main>
+  );
+}

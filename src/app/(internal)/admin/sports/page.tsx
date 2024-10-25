151f7d75519687605aaf82b2d7260761f175dbbf
Elias Nau <elias.nau@outlook.de>
2024-10-25 21:43:36 +0200

    feat: implement admin authentification

--- src/app/(internal)/admin/sports/page.tsx
+++ src/app/(internal)/admin/sports/page.tsx
@@ -3,9 +3,10 @@ import React from "react";
 
 async function Sports() {
   const user = await auth();
+  console.log(user);
 
-  if (!user || user.user.role !== "ADMIN") {
-    return <div>You need to be admin in to access this page.</div>;
+  if (!(user?.user.role === "ADMIN") || !user) {
+    return <p>You need to be an Admin to view that page</p>;
   }
 
   return <div>Sports</div>;
